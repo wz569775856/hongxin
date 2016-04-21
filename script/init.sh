1c99c30614f6c59658c96c0526dea020b9b7c969
doucharles <charlesdou@sunrisin.com>
2016-04-22 01:22:37 +0800

    add a init shell script after git clone

---
+++ script/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p ../view/static/backstage/{img,video,audio,other/mailtemplate/,css,js,fonts,common/fonts}
+mkdir -p ../view/static/frontend/{img,video,audio,other,css,js,fonts}
+npm install
