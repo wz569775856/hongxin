5f0e12284c0a5af9290501931e166d47c85f7ce8
doucharles <charlesdou@sunrisin.com>
2016-04-22 01:55:33 +0800

    update .gitignore file.

--- script/init.sh
+++ script/init.sh
@@ -2,3 +2,4 @@
 mkdir -p ../view/static/backstage/{img,video,audio,other/mailtemplate/,css,js,fonts,common/fonts}
 mkdir -p ../view/static/frontend/{img,video,audio,other,css,js,fonts}
 npm install
+git clean -x -f
