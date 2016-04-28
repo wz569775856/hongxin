82a05ec2f25f9004a5e019a3c1c13e8d1b71d357
doucharles <charlesdou@sunrisin.com>
2016-04-29 01:10:17 +0800

    chinese error code

--- index.js
+++ index.js
@@ -143,9 +143,9 @@ async.series(
                     res.locals.logo = $objConfig["logo"]
                 }
                 if ($objConfig["img_logo"]) {
-                    res.locals["img_logo"] = path.join($objConfig["staticUrl"],"multimedia/img",util.format("logo.",$objConfig["img_logo"]))
+                    res.locals["img_logo"] = path.join($objConfig["static_server_url"],"multimedia/img",util.format("logo.",$objConfig["img_logo"]))
                 }
-                res.locals["staticUrl"] = $objConfig["static_server_url"]
+                res.locals["staticUrl"] = ($objConfig["static_server_url"]=="/") ? "" : $objConfig["static_server_url"]
             }
             next()
         })
